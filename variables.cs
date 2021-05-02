9697897537a4f3a6174f901fb294fb10169fc30c
N1kom <83505815+N1kom@users.noreply.github.com>
2021-05-03 01:31:32 +0300

    Create variables.cs

---
+++ variables.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int userHeath;
+            int maxCrystalCount;
+            int userArmor;
+            float SwordDamage;
+            float userDamage;
+            string userName;
+            string groupName;
+            bool enoughCoin;
+            bool isDamageSuccess;
+            bool canDoorOpen;
+            
+
+           
+
+
+
+
+        }
+    }
+}
