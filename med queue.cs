d436c6e03db2cd6b26014d741f337bf32fa18afd
N1kom <83505815+N1kom@users.noreply.github.com>
2021-05-03 01:17:05 +0300

    Update med queue.cs

--- med queue.cs
+++ med queue.cs
@@ -5,11 +5,11 @@ namespace Learning
         static void Main(string[] args)
         {
             int peopleCount;
-            int appointedTime = 10;
+            int appointmentTime = 10; // время приёма 10 минут
             int waitingTime;
             Console.WriteLine("Вы пришли на приём к врачу и заняли место в очереди. Сколько людей стоят в очереди перед вами?");
             peopleCount = Convert.ToInt32(Console.ReadLine());
-            waitingTime = peopleCount * appointedTime;
+            waitingTime = peopleCount * appointmentTime;
             int hour = waitingTime / 60;
             int min = waitingTime % 60;
             Console.WriteLine($"Ваше время ожидания составит {hour} часов и {min} минут.");
