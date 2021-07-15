94178b7193af318797a100f072da8b43fe3b7f33
N1kom <83505815+N1kom@users.noreply.github.com>
2021-07-15 23:18:52 +0300

    Create sequence.cs

---
+++ sequence.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           //Использовал цикл for потому что заданы чёткие условия для вывода последовательности.
+            for (int i = 7; i < 100;i+=7)
+            {
+                
+                Console.WriteLine(i);
+            }
+           
+
+
+
+
+
+
+        }
+    }
+}
