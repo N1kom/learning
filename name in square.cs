16dc414723fc51b9a3dce441ef47f698800552c1
N1kom <83505815+N1kom@users.noreply.github.com>
2021-07-16 13:35:36 +0300

    Create name in square.cs

---
+++ name in square.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            char symbol;
+
+            Console.Write("Name - ");
+
+            name = Console.ReadLine();
+
+            Console.Write("Symbol - ");
+
+            symbol = Convert.ToChar(Console.ReadLine());
+
+            int lenght = name.Length+2;
+            //Console.WriteLine(lenght);
+
+            for (int i = lenght; i >=1; i--) 
+            { 
+                Console.Write(symbol);
+
+                if (i == 1)
+                {
+                    Console.WriteLine(" ");
+                    Console.WriteLine($"{symbol}{name}{symbol}");
+                }
+            }
+
+            for (int i = lenght; i >= 1; i--)
+            {
+                Console.Write(symbol);
+
+                if (i == 1)
+                {
+                    Console.WriteLine(" ");
+                    Console.ReadKey();
+                }
+
+            }
+
+            // как сделать нормально я не понял,порылся в интернете но приспособить под свою задачу не получилось,
+            // буду благодарен,если пришлёте правильное решение,что бы было понятно - как правильно.
+            
+
+
+        }
+    }
+}
